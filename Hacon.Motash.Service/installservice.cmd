87bcd83a71128038148f0f790e93f4c4bd8e964a
peter <peter@localhost>
2011-06-23 16:32:35 +0200

    added install script

---
+++ Hacon.Motash.Service/installservice.cmd
@@ -0,0 +1,2 @@
+%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\InstallUtil.exe .\Hacon.Motash.Service.exe
+:: %SystemRoot%\Microsoft.NET\Framework\v2.0.50727\InstallUtil.exe /u .\Hacon.Motash.Service.exe
