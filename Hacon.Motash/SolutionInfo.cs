7e7ce850f0baa6657b95aeac0bf45f57103924e2
Peter.Hahndorf <Peter.Boo@hahndorf.eu>
2024-07-22 20:59:57 +0200

    Changed all projects from Framework Version 4.0 to 4.8

--- Hacon.Motash/SolutionInfo.cs
+++ Hacon.Motash/SolutionInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hahndorf Consulting")]
 [assembly: AssemblyProduct("Hacon.Motash")]
-[assembly: AssemblyCopyright("Copyright © Hahndorf Consulting 2011-2014")]
+[assembly: AssemblyCopyright("Copyright © Hahndorf Consulting 2011-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.10.0")]
+[assembly: AssemblyFileVersion("2.0.11.0")]
